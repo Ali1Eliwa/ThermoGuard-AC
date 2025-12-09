e29205c16ac6585f0356939a5a783cb6faa160e2
Mahmoud Ahmed El-Adgham <ME2301530@tkh.edu.eg>
2025-12-09 08:57:34 +0200

    Update Keypad.h
    adding comments

--- code/ThermoGuard_AC/Keypad.h
+++ code/ThermoGuard_AC/Keypad.h
@@ -1,15 +1,13 @@
 /**
 *********************************************************************************
 * @file              : Keypad.h
-* @author            : Ziad Khalil, Ali Akram Ali, Mahmoud Ahmed El- Adgham
+* @author            : Ziad Khalil, Ali Akram Ali, Mahmoud Ahmed El-Adgham
 * @brief             : Header file for the 5-button analog keypad driver.
 *********************************************************************************
 */
 
-
 #ifndef KEYPAD_H
 #define KEYPAD_H
-
 
 /* ****************** Macro Section Start ********************** */
 // Key press definitions (Returned by Get_Keypad_Press)
@@ -24,10 +22,10 @@ enum {
 /* ****************** Macro Section End ************************ */
 
 
-
 /* ****************** Sup-Program Decleration Section Start **** */
 /**
-* @brief Reads the analog voltage from the keypad's ADC channel.
+* @brief Reads the analog voltage from the keypad's ADC channel
+* : and maps it to a specific key.
 * @return The corresponding key value (e.g., KEY_UP) or KEY_NONE.
 */
 int Get_Keypad_Press(void);
